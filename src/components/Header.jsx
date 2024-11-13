3601d667bfd38bf0f47536fd826420ec1c85a7de
Your Name <golamsarwar1896@gmail.com>
2024-11-14 01:29:14 +0600

    routes are implemented. Worked with Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/logo.png";
+const Header = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-2 p-5">
+      <div className="logo">
+        <img className="w-96" src={logo} alt="Logo" />
+      </div>
+      <h2 className="text-zinc-400">Journalism Without Fear or Favour</h2>
+    </div>
+  );
+};
+
+export default Header;
